a208218cb2ce6955f64a8ff2f6a5d2cc5ccd55cb
Miles McBain <miles.mcbain@gmail.com>
2025-08-18 20:39:39 +1000

    export more_message()
    Fixes #1

--- R/more_message.R
+++ R/more_message.R
@@ -1,4 +1,13 @@
-# constructor for verbose messages definded in-line.
+#' Constructor for more messages definded in-line.
+#'
+#' This is a simple helper for defining messages with a title and body using
+#' formatting from `{cli}`.
+#'
+#' Messages can be any text, so you are not limited to using this function to
+#' create them.
+#' @param title the title of message
+#' @param body the rest of the message
+#' @export
 more_message <- function(
   title,
   body
